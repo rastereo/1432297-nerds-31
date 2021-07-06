fe839fedf73e5ddc795359de5c8f25eba941b066
rastereo <kaptur-rostislav@mail.ru>
2021-07-06 19:25:46 +0300

    Исправил ошибки на внутренней странице

---
+++ script.js
@@ -0,0 +1,5 @@
+let sliderControls = document.querySelector('.slider-controls');
+let button2 = document.querySelector('.button2');
+button.onclick = function() {
+    page.classList.toggle('current');
+};
