a84501f5094905aba21feec113d38b721908ab7c
rastereo <kaptur-rostislav@mail.ru>
2021-07-15 22:00:29 +0300

    Подготовка к 2-ой попытки

--- js/popup.js
+++ js/popup.js
@@ -1,25 +1,40 @@
 let contactsButton = document.querySelector('.contacts-text .button');
-let popup = document.querySelector('.modal-email');
+let popup = document.querySelector('.modal');
 let focusImput = popup.querySelector('[name=name]');
 let buttonClose = popup.querySelector('.button-close');
+let loginForm = popup.querySelector('.email-form');
+let loginName = popup.querySelector(".login-name");
+let loginEmail = popup.querySelector(".login-email");
+let loginText = popup.querySelector('.login-text');
 
 
 contactsButton.addEventListener("click", function (evt) {
     evt.preventDefault();
-    popup.classList.remove('visually-hidden');
+    popup.classList.add('modal-show');
     focusImput.focus();
 });
 
 buttonClose.addEventListener("click", function (evt) {
     evt.preventDefault();
-    popup.classList.add('visually-hidden');
+    popup.classList.remove('modal-show');
+    popup.classList.remove("modal-error");
 });
 
 window.addEventListener('keydown', function (evt) {
     if (evt.key === 'Esc' || evt.key === 'Escape') {
-        if (!popup.classList.contains('visually-hidde')) {
+        if (popup.classList.contains('modal-show')) {
             evt.preventDefault();
-            popup.classList.add('visually-hidden')
+            popup.classList.remove('modal-show');
+            popup.classList.remove("modal-error");
         }
+    }
+});
+
+loginForm.addEventListener("submit", function (evt) {
+    if (!loginName.value || !loginEmail.value || !loginText.value) {
+        evt.preventDefault();
+        popup.classList.remove("modal-error");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("modal-error");
     }
 });
